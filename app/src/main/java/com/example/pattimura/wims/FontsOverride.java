5380c00a04763b41dcdd22c136a2ab445f854515
grahamdesmon <graham.desmon.simanjuntak@gmail.com>
2017-03-04 03:21:00 +0700

    update chat function

---
+++ app/src/main/java/com/example/pattimura/wims/FontsOverride.java
@@ -0,0 +1,32 @@
+package com.example.pattimura.wims;
+
+import java.lang.reflect.Field;
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by desmoncode on 04/03/17.
+ */
+
+public class FontsOverride {
+    public static void setDefaultFont(Context context,
+                                      String staticTypefaceFieldName, String fontAssetName) {
+        final Typeface regular = Typeface.createFromAsset(context.getAssets(),
+                fontAssetName);
+        replaceFont(staticTypefaceFieldName, regular);
+    }
+
+    protected static void replaceFont(String staticTypefaceFieldName,
+                                      final Typeface newTypeface) {
+        try {
+            final Field staticField = Typeface.class
+                    .getDeclaredField(staticTypefaceFieldName);
+            staticField.setAccessible(true);
+            staticField.set(null, newTypeface);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
